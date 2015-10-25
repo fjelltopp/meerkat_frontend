67d38959a3363318ce705d87f628bf39bbf38e65
Kenrick Turner <kenrick@kenners.org>
2015-10-25 11:29:53 +0000

    Add Hello world index page

--- meerkat_frontend/__init__.py
+++ meerkat_frontend/__init__.py
@@ -7,12 +7,18 @@ Root Flask app for the Meerkat frontend.
 This module runs as the root Flask app and mounts component Flask apps for
 different services such as the API and Reports.
 """
-from flask import Flask
+from flask import Flask, jsonify, abort, render_template, url_for, request
+
 # Create default app
 app = Flask(__name__)
 
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
 # Import modules
-#import meerkat_frontend.reports
+# import meerkat_frontend.reports
 
 # Main
 if __name__ == "__main__":
