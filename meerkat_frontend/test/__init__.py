62294bf1dcd9fa58d3784ae868cb245dce7182e5
Kenrick Turner <kenrick@kenners.org>
2015-12-06 18:58:19 +0000

    Fix tests

--- meerkat_frontend/test/__init__.py
+++ meerkat_frontend/test/__init__.py
@@ -28,15 +28,15 @@ class MeerkatFrontendTestCase(unittest.TestCase):
     def test_reports(self):
         """Check the Reports page loads"""
         rv = self.app.get('/reports/')
-        self.assertIn(rv.status_code, [200, 302])
+        self.assertIn(rv.status_code, [200, 301, 302])
 
     def test_reports_jor_pub_health(self):
         rv = self.app.get('/reports/test/jordan/public_health')
-        self.assertIn(rv.status_code, [200, 302])
+        self.assertIn(rv.status_code, [200, 301, 302])
 
     def test_reports_jor_cd(self):
         rv = self.app.get('/reports/test/jordan/communicable_diseases')
-        self.assertIn(rv.status_code, [200, 302])
+        self.assertIn(rv.status_code, [200, 301, 302])
 
     def test_technical(self):
         """Check the Technical page loads"""
