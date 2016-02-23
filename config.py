22e61220ac64f9712e1304b925562cccd0213cee
mrjb <jonathan.s.berry@googlemail.com>
2016-02-23 14:06:14 +0000

    Added from_env method to config file

--- config.py
+++ config.py
@@ -3,6 +3,21 @@ config.py
 
 Configuration and settings
 """
+import os
+
+def from_env(env_var, default):
+    """ Gets value from envrionment variable or uses default
+
+    Args: 
+        env_var: name of envrionment variable
+        default: the default value
+    """
+    new = os.environ.get(env_var)
+    if new:
+        return new
+    else:
+        return default
+
 
 class Config(object):
     DEBUG = False
@@ -11,8 +26,11 @@ class Config(object):
     INTERNAL_API_ROOT = 'http://dev_nginx_1/api'
     EXTERNAL_API_ROOT = '/api'
     HERMES_ROOT = 'https://hermes.aws.emro.info'
+    HERMES_API_KEY = from_env( 'HERMES_API_KEY', 'test-hermes' )
     USERNAME = "admin"
     PASSWORD = "secret"
+
+    
 
 class Production(Config):
     DEBUG = False
