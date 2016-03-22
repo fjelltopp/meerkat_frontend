bad01aaecd5399cd07a7585a2d1fdd5fc9e92e32
Gunnar Ro <gunnarroe@gmail.com>
2016-03-22 10:25:20 +0000

    Now working with boostrap tables

--- meerkat_frontend/src/js/explore/explore.js
+++ meerkat_frontend/src/js/explore/explore.js
@@ -4,7 +4,7 @@
 //Any optional analysis (stripping records/colouring/graphing) can then be applied.
 // start_date and end_date are optional arguments
 //Details for this analysis are given in the options object.
-//Finally the table is drawn. 
+//Finally the table is drawn.
 function createCrossPlot( catx, caty, options, start_date, end_date ){
 	//These variable will hold all the JSON data from the api, when the AJAX requests are complete.
 	var queryData, catxData, catyData;
@@ -32,92 +32,123 @@ function createCrossPlot( catx, caty, options, start_date, end_date ){
 
 	//Run the AJAX reuqests asynchronously and act when they have all completed.
 	$.when.apply( $, deferreds ).then(function() {
-
-
+		
+		//Sort out the data for the table
 		if( $.isEmptyObject(queryData) ){
 			console.log("Empty object");
 			$('.exploreChart .table-responsive').html( 
 				"Unfortunately we can't plot that table.  Please try another combination of categories." 
 			);
 		}else{
-
 			//Store the variable id's for each category.
 			var yKeys = Object.keys(queryData);
 			var xKeys = Object.keys(queryData[yKeys[0]]);
 
-			//Get the "names" corresponding to each variable ID in the x category.
-			//These names, preceeded by an empty box, form the top row of the table.
-			var xKeyNames = ["#Cases"];
-			for( var k in xKeys ){
-				xKeyNames.push( xKeys[k] );
-			}
-			var table = [ xKeyNames ];
-
+			var columns = [
+				{
+					"field": "state",
+					"checkbox": true
+				},{
+					"field": "cases",
+					"title": "#Cases",
+					"align": "left",
+					"class": "header"
+				},
+					
+			];
+			
+			var data = [];
 			//For each key in the y category, form the row from x category data.
-			for( var y in yKeys ){
-			
-				var yKey = yKeys[y];
-			
-				//Each row begins with the variable name.
-				var row = [yKey];
-
-				//The rest of the row are the correctly ordered data points.
+			for( var y in yKeys.sort() ){
+				var datum ={
+					"cases": timelineLink(yKeys[y],catyData[yKeys[y]].name,"y")
+				};
+				var total = 0;
 				for( var x in xKeys ){
-
-					var xKey = xKeys[x];
-					var datum = queryData[yKey][xKey];				
-								
-					row.push( datum );		
-				
+					datum[xKeys[x]] = queryData[yKeys[y]][xKeys[x]];
+					total +=queryData[yKeys[y]][xKeys[x]];
 				}
-
-				table.push( row );
-
+				datum.total = total;
+				data.push( datum );		
 			}
-		
 			//Now that we have the data in a form javascript can understand, we can do fancy stuff with it.
+			colour = false;
 			if( options ){
-				if( options.strip ) table = strip( table, options.strip );
-				if( options.colour == "true" ) table = colour( table );
+				if( options.strip ){
+					var r = strip(columns, data, options.strip );
+					columns = r[0];
+					data = r[1];
+				}
+				if( options.colour == "true" ){
+					maxMin = max_min( data );
+					colour = true;
+				}
 			}
-
-			//Draw!
-			drawCrossPlot( table, catxData, catyData  );
+			// Add the rest of the columns when we know if we are colouring in the cells
+			for( var k in xKeys.sort() ){
+				var column ={
+					"field": xKeys[k],
+					"title": timelineLink(xKeys[k],catxData[xKeys[k]].name,"x"),
+					"sortable": true
+					
+				};
+				if(colour){
+					column.cellStyle= createColourCell(maxMin);
+				}
+				columns.push(column);
+			}
+			columns.push(
+				{
+					"field": "total",
+					"title": "Total",
+					sortable: true
+				}
+			);
+			//Draw the table
+			$("#cross-table").bootstrapTable("destroy");
+			$("#cross-table").bootstrapTable({
+				columns: columns,
+				data: data,
+				clickToSelect: true
+			});
+			console.log(columns);
+			console.log(data);
 		}
 	});
 }
 
-//A function that colours the table, to help bring out patterns and information.
-function colour( jsTable ){
-
-	//First find the highest and lowest values in the table. 
-	var list = [];
-	for( var r=1; r<jsTable.length; r++ ){
-		row = jsTable[r].slice();
-		row.shift();
-		list = list.concat( row );
+function createColourCell(maxMin){
+	// Returns a function that colours in the cells according to their value
+	function cc(value, row, index){
+		var perc = (value-maxMin[0])/(maxMin[1]-maxMin[0]);
+		return {css: {"background-color": "rgba(217, 105, 42, " + perc +")"}};
 	}
+	return cc;
+}
 
+function max_min(data){
+	// Returns max and min of all numbers in table
+	var list = [];
+	for( var r=1; r<data.length; r++ ){
+		var row = data[r];
+		for(var x in row){
+			if( x!= "cases" && x!= "total"){
+				list.push(Number(row[x]));
+			}
+		}
+	}
 	var maximum = Math.max.apply( Math, list );
 	var minimum = Math.min.apply( Math, list );
-
-	for( var y=1; y< jsTable.length; y++ ){
-		for( var x=1; x<jsTable[y].length; x++ ){
-			var perc = (jsTable[y][x]-minimum)/(maximum-minimum);
-			jsTable[y][x] = [ jsTable[y][x], perc ]; 
-		}	
-	}
-
-	return jsTable;
-
+	return [minimum,maximum];
 }
+
 
 function timelineLink(id, name, axis){
 	//helper function to create links to activate the timeline
 	return '<a href="#" onclick="prepareExploreTimeline(&apos;' + id + '&apos;, &apos;' + axis +'&apos;);" class="cross-table-links">' + name + "</a>";
 }
 
-function createTimeline(id, category, start_date, end_date){
+function createTimeline(id, category, options, start_date, end_date){
 	if( start_date !== undefined && end_date !== undefined){
 		main_query = $.getJSON( api_root + '/query_variable/' + id + '/' + category + '/' + start_date + '/' + end_date + '?use_ids=1', function(data) {
 			queryData = data;
@@ -138,77 +169,76 @@ function createTimeline(id, category, start_date, end_date){
 	];
 	//Run the AJAX reuqests asynchronously and act when they have all completed.
 	$.when.apply( $, deferreds ).then(function() {
-		drawTimeline(queryData, categories, variable);
+		var yKeys = Object.keys(queryData);
+		var xKeys = Object.keys(queryData[yKeys[0]].weeks);
+		var columns = [
+			{
+				"field": "cases",
+				"title": "#Cases with "+ variable.name,
+				"align": "left",
+				"class": "header"
+			}
+		];
+		var data = [];
+		//For each key in the y category, form the row from x category data.
+		for( var y in yKeys.sort() ){
+			var datum ={
+				"cases": categories[yKeys[y]].name
+			};
+			var total = 0;
+			for( var x in xKeys ){
+				datum["week_" + xKeys[x]] = queryData[yKeys[y]].weeks[xKeys[x]];
+				total += queryData[yKeys[y]].weeks[xKeys[x]];
+			}
+			datum.total = total;
+			data.push( datum );		
+		}
+		// Sort out options
+		colour = false;
+		if( options ){
+			if( options.strip ){
+				var r = strip(columns, data, options.strip );
+				columns = r[0];
+				data = r[1];
+			}
+			if( options.colour == "true" ){
+				maxMin = max_min( data );
+				colour = true;
+			}
+		}
+		// Add remaining columns
+		for( var k in xKeys.sort(function(a, b){return a-b;}) ){
+			var column ={
+				"field": "week_"+ xKeys[k],
+				"title": "Week " + xKeys[k],
+				"sortable": true
+			};
+			if(colour){
+				column.cellStyle= createColourCell(maxMin);
+			}
+			columns.push(column);
+		}
+		columns.push(
+			{
+				"field": "total",
+				"title": "Total",
+				sortable: true
+			}
+		);
+		//Draw!
+		$("#timeline-table").bootstrapTable("destroy");
+		$("#timeline-table").bootstrapTable({
+			columns: columns,
+			data: data
+		});
+		
 	});
 
 
 	
 }
 
-function drawTimeline (data, categories, variable){
-	// A timeline with weeks across the x-axis and categories down the y axis
-	var table = "<table class='table table-hover table-responsive'>";
-	var ycats = Object.keys(data);
-	var weeks = Object.keys(data[ycats[0]].weeks);
-	table += "<tr> <th> Cases with "+ variable.name + "</th>";
-	for (var week in weeks){
-		table += "<th> Week " + weeks[week] + "</th>";
-	}
-	table += '<th class="border-left"> Total </th> </tr>';
-	for(var c in ycats){
-		var cat = ycats[c];
-		table += '<tr> <td class="header">' + categories[cat].name + "</td>";
 
-		for(week  in weeks){
-			table += "<td>" + data[cat].weeks[weeks[week]] + "</td>";
-		}
-		
-		table += '<td class="border-left">' + data[cat].total +"</td></tr>";
-	}
-	$("#timeline-chart").html(table);
-}
-
-function drawCrossPlot( jsTable, catxData, catyData){
-
-	var table = "<table class='table table-hover'>";
-	console.log(jsTable);
-	//We draw html tables by rows, not columns, so begin by looping through y.
-	for( var y in jsTable ){
-
-		var row = jsTable[y];
-
-		//If first row, draw as headers. We want to add links to each header to show more detailed time line information
-		if( Number(y) === 0 ){
-			table += "<tr>";
-			for( var h in row ){
-				if( Number(h) === 0 ) table += "<th class='header'>" + row[h] + "</th>";
-				else table += "<th>" + timelineLink(row[h], catxData[row[h]].name, "x") + "</th>";
-			}
-			table += "</tr>";
-		}else{
-
-			table += "<tr><td class='header'>" + timelineLink(row[0], catyData[row[0]].name, "y") + "</td>";
-			//Loop through x, drawing a new cell for each data point.
-			for( var x=1; x<row.length; x++ ){
-				//If this is a three dimensional data set, use colour to indicate the third dimension.
-				if( jsTable[y][x].constructor === Array ){
-					table += "<td style='background-color:rgba(217, 105, 42, " + 
-					         jsTable[y][x][1] + ")'>" + jsTable[y][x][0] + "</td>";
-				}else{
-					table += "<td>" + jsTable[y][x] + "</td>";
-				}
-			}
-			table += "</tr>";
-
-		}
-
-	} 
-
-	table += "</table>";
-	
-	$('.exploreChart .table-responsive').html(table);
-
-}
 
 function transpose( jsTable ){
 	var newTable = jsTable[0].map(function(col, i) { 
@@ -219,40 +249,39 @@ function transpose( jsTable ){
 	return newTable;
 }
 
-function strip( table, axis ){
+function strip( columns, data, axis ){
 
-	function stripRows(jsTable){
+	function stripRows(data){
+		console.log("StripRows");
 		//Store a list of rows to be removed.
 		var remove = [];
-
 		//For each row iterate through it's elements to seeif all are empty.
-		for( var y=1; y<jsTable.length; y++ ){
-
-			var row = jsTable[y].slice();
-			row.shift();
+		for( var y=1; y<data.length; y++ ){
+			var row = data[y];
 			var empty = true;
-			
 			for( var x in row ){
-				if( Number(row[x]) !== 0 ) empty = false;
+				if( x != "cases" && Number(row[x]) !== 0 ) empty = false;
 			}
-			if( empty ) remove.push(y);
+			if( empty ) {
+				remove.push(y);
+			}
 		}
+		for( var i = remove.length-1; i>=0; i-- ) data.splice( remove[i], 1 );
+		console.log(data.length);
 		//Remove all empty rows (starting from the last to avoid screwing up indexes).
-		for( var i = remove.length-1; i>=0; i-- ) jsTable.splice( remove[i], 1 );
-
-		return jsTable;
+		return data;
 	}
 
 
-	if( axis.indexOf('y') != -1 || axis.indexOf('Y') != -1 ){
-		table = stripRows( table );
-	}
+	// if( axis.indexOf('y') != -1 || axis.indexOf('Y') != -1 ){
+	// 	table = stripRows( table );
+	// }
 	
-	if( axis.indexOf('x') != -1 || axis.indexOf('X') != -1 ){
-		table = transpose( stripRows( transpose( table ) ) );
-	}
-	
-	return table;
+	// if( axis.indexOf('x') != -1 || axis.indexOf('X') != -1 ){
+	// 	table = transpose( stripRows( transpose( table ) ) );
+	// }
+	data = stripRows(data);
+	return [columns, data];
 
 }
 
