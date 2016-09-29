f9b19c4687017f7ca475f5d600e11c6b44f6eca7
mrjb <jonathan.s.berry@googlemail.com>
2016-09-29 12:52:45 +0100

    Cross-domain login request now made server side.

--- meerkat_frontend/views/homepage.py
+++ meerkat_frontend/views/homepage.py
@@ -5,6 +5,7 @@ A Flask Blueprint module for the homepage.
 """
 from flask import Blueprint, render_template, current_app, abort, g, request
 from flask.ext.babel import get_translations, gettext
+import requests
 
 homepage = Blueprint('homepage', __name__,url_prefix='/<language>')
 
@@ -23,3 +24,17 @@ def login():
         content=current_app.config['HOMEPAGE_CONFIG'],
         redirect=url
     )
+
+@homepage.route('/login_request', methods=['POST'])
+def login_request():
+    """
+    Make a login request to the authentication module.
+    Can't do this directly from the browser because of the "same-origin policy".
+    Browser scripts can't make cross domain POST requests.
+    """
+    url = current_app.config['AUTH_ROOT'] + "/api/login"
+    current_app.logger.warning(url)
+    current_app.logger.warning( request.json )
+    r = requests.post( url, json = request.json ) 
+    current_app.logger.warning(r)
+    return (r.text, r.status_code, r.headers.items())
