843d504db7730a8aa1206c5bceccf7d1f3f10b5d
mrjb <jonathan.s.berry@googlemail.com>
2016-10-12 15:14:54 +0100

    Mock check_auth for tests.

--- meerkat_frontend/test/__init__.py
+++ meerkat_frontend/test/__init__.py
@@ -8,6 +8,11 @@ import meerkat_frontend as mk
 import base64, unittest, calendar, jwt, os, time
 from datetime import datetime
 from werkzeug.datastructures import Headers
+from unittest.mock import MagicMock
+from unittest import mock
+from flask import g
+import authorise as auth
+
 #Check if auth requirements have been installed
 try:
     #Test by importing package that will only ever be required in auth (touch wood). 
@@ -30,28 +35,31 @@ class MeerkatFrontendTestCase(unittest.TestCase):
         """Setup for testing"""
         mk.app.config['TESTING'] = True
         self.app = mk.app.test_client()
- 
-        #We need to authenticate our tests using the dev/testing rsa keys. 
-        token_payload = {
-            u'acc': {
-                u'demo': [u'root',u'admin',u'registered'], 
-                u'jordan': [
-                    u'root', u'central',u'directorate',u'clinic',u'reports',
-                    u'all',u'cd',u'ncd',u'mh',u'admin',u'personal'
-                ], 
-                u'madagascar':[u'root',u'admin',u'registered'],
-                u'rms':[u'root',u'admin',u'registered']
-            }, 
-            u'data': {u'name': u'Testy McTestface'}, 
-            u'usr': u'testUser', 
-            u'exp': calendar.timegm( time.gmtime() ) + 1000,  #Lasts for 1000 seconds
-            u'email': u'test@test.org.uk'
-        }
-        token = jwt.encode(token_payload, JWT_SECRET_KEY, algorithm=JWT_ALGORITHM)
-        self.header = { 'Authorization': JWT_HEADER_PREFIX + token.decode("utf-8") }
+
+        #When check_auth is called, just allow the authentication and set a permissive payload.
+        def side_effect(roles, countries):
+            g.payload = {
+                u'acc': {
+                    u'demo': [u'root',u'admin',u'registered'], 
+                    u'jordan': [
+                        u'root', u'central',u'directorate',u'clinic',u'reports',
+                        u'all',u'cd',u'ncd',u'mh',u'admin',u'personal'
+                    ], 
+                    u'madagascar':[u'root',u'admin',u'registered'],
+                    u'rms':[u'root',u'admin',u'registered']
+                }, 
+                u'data': {u'name': u'Testy McTestface'}, 
+                u'usr': u'testUser', 
+                u'exp': calendar.timegm( time.gmtime() ) + 30,  #Lasts for 30 seconds
+                u'email': u'test@test.org.uk'
+            }
+
+        #Mock check_auth method. Authentication should be tested properly else where. 
+        self.patcher = mock.patch('authorise.check_auth', side_effect = side_effect)
+        self.mock_auth = self.patcher.start()
         
     def tearDown(self):
-        pass
+        self.patcher.stop()
 
     def test_index(self):
         """Check the index page loads"""
@@ -61,48 +69,42 @@ class MeerkatFrontendTestCase(unittest.TestCase):
 
     def test_reports(self):
         """Check the Reports page loads"""
-        rv = self.app.get('/en/reports/')
-        #Without auth you should be redirected to login page.
-        self.assertIn(rv.status_code, [302])
-        #With auth you should get a 200 ok  
-        rv2 = self.app.get('en/reports/', headers=self.header)
+        rv = self.app.get('/en/reports/') 
+        rv2 = self.app.get('en/reports/')
         self.assertEqual(rv2.status_code, 200)
 
     def test_reports_pub_health(self):
-        rv = self.app.get('en/reports/test/public_health/',headers=self.header)
+        rv = self.app.get('en/reports/test/public_health/' )
         self.assertIn(rv.status_code, [200])
         self.assertIn(b"5,941 consultations", rv.data)
         self.assertIn(b"Viral infections characterized by skin and mucous membrane lesions", rv.data)
 
     def test_reports_cd_pub_health(self):
-        rv = self.app.get('en/reports/test/cd_public_health/',headers=self.header)
+        rv = self.app.get('en/reports/test/cd_public_health/' )
         self.assertIn(rv.status_code, [200])
         self.assertIn(b"73 cases", rv.data)
         self.assertIn(b"Viral infections characterized by skin and mucous membrane lesions", rv.data)
 
     def test_reports_ncd_pub_health(self):
-        rv = self.app.get('en/reports/test/ncd_public_health/',headers=self.header)
+        rv = self.app.get('en/reports/test/ncd_public_health/' )
         self.assertIn(rv.status_code, [200])
         self.assertIn(b"64 cases", rv.data)
         self.assertIn(b"Other disorders of glucose regulation and pancreatic internal secretion", rv.data)
 
     def test_reports_cd(self):
-        rv = self.app.get('en/reports/test/communicable_diseases/',headers=self.header)
+        rv = self.app.get('en/reports/test/communicable_diseases/' )
         self.assertIn(rv.status_code, [200])
         self.assertIn(b"There were 0 new confirmed cases and 1 new suspected cases of Bloody diarrhoea", rv.data)
 
     def test_reports_ncd(self):
-        rv = self.app.get('en/reports/test/non_communicable_diseases/',headers=self.header)
+        rv = self.app.get('en/reports/test/non_communicable_diseases/' )
         self.assertIn(rv.status_code, [200])
         self.assertIn(b"with <br> hypertension", rv.data)
         
     def test_technical(self):
         """Check the Technical page loads"""
-        rv = self.app.get('/en/technical/')
-        #Without auth you should be redirected to login page.
-        self.assertEqual(rv.status_code, 302)
-        #With auth you should get a 200 ok        
-        rv2 = self.app.get('/en/technical/', headers=self.header)
+        rv = self.app.get('/en/technical/')       
+        rv2 = self.app.get('/en/technical/')
         self.assertEqual(rv2.status_code, 200)
 
     #HOMEPAGE testing
