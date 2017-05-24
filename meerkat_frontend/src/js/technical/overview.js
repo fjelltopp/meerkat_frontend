36a0fadaea56c41495bc2dbe3e419397ec82ab3d
Ziad Abu Ghazaleh <ziad0>
2017-05-24 19:51:14 +0300

    Updates on the Overview page

--- meerkat_frontend/src/js/technical/overview.js
+++ meerkat_frontend/src/js/technical/overview.js
@@ -24,8 +24,9 @@ function html_box_builder(overviewObj, locID) {
     $('#divOverviewContent').append(html_builder);
 
     $.each(overviewObj.contents, function(index, value) {
-    //Build the content...
-        window[overviewObj.prep_function](value, overviewObj.parentId, locID);
+        //Build the content...
+        //if ()
+        window[value.prep_function](value, overviewObj.parentId, locID);
     });
 
 }
@@ -33,76 +34,28 @@ function html_box_builder(overviewObj, locID) {
 
 
 function prep_row(contentsObj, parentId, locID) {
-    
-        var api_element = //TODO: Assign a value;
-            //Append the results ...
-        var htmlRow = "<div class='divTableRow'>" +
-            "<div class='divTableCell'> " + contents_obj.label + "</div>" +
-            "<div class='divTableCell" + api_element + "'>Loading</div>" +
-            "</div>";
-    
-        $("#" + parentId).append(htmlRow);
-    
-        // Get the inner value for the boxes by calling the APIs ...
-        var apiUrl = contentsObj.api.replace("<loc_id>", locID);
-        $.getJSON(api_root + apiUrl, function(data) {
 
-            $('#' + parentId + ' .' + api_element).html(data.value);
-
-        });
+    //Generate a GUID ...
+    var api_element = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0,
+            v = c == 'x' ? r : r & 0x3 | 0x8;
+        return v.toString(16);
     });
+
+    //Append the results ...
+    var htmlRow = "<div class='divTableRow'>" +
+        "<div class='divTableCell'> " + contentsObj.label + "</div>" +
+        "<div class='divTableCell " + api_element + "'>Loading..</div>" +
+        "</div>";
+
+    $("#" + parentId).append(htmlRow);
+
+    // Get the inner value for the boxes by calling the APIs ...
+    var apiUrl = contentsObj.api.replace("<loc_id>", locID);
+    $.getJSON(api_root + apiUrl, function(data) {
+
+        $('#' + parentId + ' .' + api_element).html(data.value);
+
+    });
+
 }
-
-
-
-
-
-//
-// function html_box_builder(currentObj, locID) {
-//     var overview_api_result = [];
-//     var overview_api_counter = 0;
-//     var html_builder = "";
-//
-//     //Build the header for the box ...
-//     html_builder = html_builder + "<div class='col-xs-12 " + currentObj.html_class + " less-padding-col'> <div class='chartBox box' >" +
-//         "<div class = 'chartBox__heading' > <p id = '#box_heading'>" + currentObj.title + "</p> </div>" +
-//         "<div class = 'chartBox__content' > " +
-//         "<div id = 'use-table' class = 'table' >" +
-//         "<table class = 'table table-hover table-condensed'> <tbody> ";
-//
-//     //Build the body for the box ...
-//     $.each(currentObj.contents, function(index, value) {
-//         overview_api_counter = overview_api_counter + 1;
-//
-//         html_builder = html_builder + "<tr><td>" + value.label + "</td> <td> " + value.api + " </td> </tr>";
-//
-//         // Get the inner value for the boxes by calling the APIs ...
-//         var apiUrl = value.api.replace("<loc_id>", locID);
-//         $.getJSON(api_root + apiUrl, function(data) {
-//
-//             var apiValue;
-//             if (typeof data == 'undefined') {
-//                 apiValue = 0;
-//             } else {
-//                 apiValue = data.value;
-//             }
-//
-//             //fill the array ...
-//             overview_api_result.push({
-//                 api_url: value.api,
-//                 api_data: apiValue
-//             });
-//
-//             //Check if the Json is ready and the data are Pushed ...
-//             if (overview_api_result.length === currentObj.contents.length) {
-//                 $.each(overview_api_result, function(index, value) {
-//                     html_builder = html_builder.replace(value.api_url, value.api_data);
-//                 });
-//                 $('#div_Overview_Content').append(html_builder);
-//             }
-//         });
-//     });
-//
-//     //Build the box footer ...
-//     html_builder = html_builder + "</tbody> </table> </div> </div> </div> </div>";
-// }
