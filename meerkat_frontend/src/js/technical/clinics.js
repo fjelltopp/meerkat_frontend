60cfd125006828b029e3ebc87d617914be11af05
Ziad Abu Ghazaleh <ziad0>
2017-07-22 18:32:17 +0300

    This is the code that responsible for building the clinic tab

--- meerkat_frontend/src/js/technical/clinics.js
+++ meerkat_frontend/src/js/technical/clinics.js
@@ -1,6 +1,8 @@
 //Global Variable ...
-var globalArray = [];
-var weeksArray = [];
+
+var columnNameArray = [];
+var clinicDataArray = [];
+
 
 // This will be the main function for the Clinics Page...
 function build_clinics_page(locID) {
@@ -10,166 +12,197 @@ function build_clinics_page(locID) {
 
 
 function GatherClinicsData(locID) {
+    //Clear the arrays ...
+    columnNameArray = [];
+    clinicDataArray = [];
+
+
     //API URLS to get Clinisc Data ...
-    var clinicsNewDate = '/query_variable/vis_1/locations:clinic?only_loc=' + locID;
-    var clinicsReturnDate = '/query_variable/vis_2/locations:clinic?only_loc=' + locID;
+    var clinicsNewData = '/query_variable/vis_1/locations:clinic?only_loc=' + locID + '&use_ids=True';
+    var clinicsReturnData = '/query_variable/vis_2/locations:clinic?only_loc=' + locID + '&use_ids=True';
+    var completenessData = '/completeness/reg_1/1/6?sublevel=clinic';
+    var locationAPI = "/locations";
+
+
+    BuildClinicTable();
 
     //Call the first API that return the new visits ..
-    $.getJSON(api_root + clinicsNewDate, function(data) {
+    $.getJSON(api_root + clinicsNewData, function(data) {
         $.each(data, function(index, value) {
-            //Add clinics name ..
-            globalArray.push(index);
-            //Arrange the data into one global Objects ...
-            BuildClinicNewVisitsDS(index, value.weeks);
+            BuildClinicNewVisits(index, value.weeks);
         });
 
 
         //Call the return API after finishing the arrange process for the "new clinic visits" ...
-        $.getJSON(api_root + clinicsReturnDate, function(data) {
+        $.getJSON(api_root + clinicsReturnData, function(data) {
             $.each(data, function(index, value) {
-                //Arrange the data into one global Objects ...
-                BuildClinicReturnVisitsDS(index, value.weeks);
+                BuildClinicReturnVisits(index, value.weeks);
             });
 
-            //Build The HTML Content ...
-            BuildClinicHTMLContent();
 
+            //Get the completeness data ...
+            $.getJSON(api_root + completenessData, function(dataCom) {
+                $.each(dataCom.timeline, function(index, value) {
+                    BuildClinicCompleteness(index, value.values);
+                });
+
+                //Get the reall name for the clinics and replace the numbers with the text ...
+                $.getJSON(api_root + locationAPI, function(dataLocation) {
+                    GetClinicsNames(dataLocation);
+
+                    //Append the table and the columns ...
+                    var bHtml = "<table id='clinicsTable' data-toggle='Table'  data-export='true' data-search='true'  class='table table-hover table-condense' style='background-color:white;'></table>";
+
+                    $('#divClinicContent').append(bHtml);
+                    $('#clinicsTable').bootstrapTable({
+                        columns: columnNameArray,
+                        data: clinicDataArray,
+                        pagination: true,
+                        pageSize: 10
+                    });
+
+                    //Update the table header so there is no need for scroll bar ...
+                    ChangeHeaderCss();
+                });
+            });
         });
     });
-
-
-
 }
 
-//This method will arrange the data into one global object to make it easy to draw into table and rows ...
-function BuildClinicNewVisitsDS(clinicName, weeks) {
-    var counter = 0;
-    for (var week in weeks) {
-        var weekName = week;
 
-        //Save the week number into a weeksArray so i can know all the weeks from the new & return visit + i can easly arrange them ...
-        //If the week number not exist then i will add it so i will have only unique numbers ...
-        var wResult = $.inArray(weekName, weeksArray);
-        if (wResult === -1) {
-            weeksArray.push(weekName);
+
+function BuildClinicTable() {
+    //Add the first Column ...
+    BuildTableColumn("clinicName", "Name Of Facility", "black");
+
+    //Get the current week with the last two weeks, 3 weeks total ......
+    var currentWeek = week;
+    for (i = week - 2; i <= currentWeek; i++) {
+
+        var columnRedStyle = "black";
+        var currentText = "";
+        if (i === week) {
+            columnRedStyle = "red";
+            currentText = " current ";
         }
+        //Draw the clolumns depend on the week number ...
+        BuildTableColumn(i + "N", "Week " + i + currentText + " (new) ", columnRedStyle);
+        BuildTableColumn(i + "F", "Week " + i + currentText + " (follow up) ", columnRedStyle);
+    }
 
-        //Add the week Number into the first index ...
-        weekName = [weekName];
-        //Add the Week new Value into the second index ...
-        weekName.push(weeks[week]);
-
-
-        if (counter === 0) {
-            globalArray[clinicName] = [];
-            globalArray[clinicName].push(weekName);
-        } else {
-            globalArray[clinicName].push(weekName);
+    //Now after adding the new and return columns i will add the Completeness column ...
+    for (i = week - 2; i <= currentWeek; i++) {
+        var columnColor = "black";
+        var currentTxt = "";
+        if (i === week) {
+            columnColor = "red";
+            currentTxt = " current ";
         }
-        counter = counter + 1;
-
+        BuildTableColumn(i + "C", "Week " + i + currentTxt + " (Completeness) ", columnColor);
     }
 }
 
-function BuildClinicReturnVisitsDS(clinicName, weeks) {
-    var newVisit = globalArray[clinicName];
-    $.each(newVisit, function(index, newVisitArray) {
-        //get the week number ...
-        var weekName = newVisitArray[0];
-        //Check if the week number from NewVists exist in the returnVist then add the value else add 0 ...
-        if (weeks.weekName === undefined) {
-            newVisitArray.push(0);
 
-            //Check if the weeks obj contains data ...
-            if (Object.keys(weeks).length > 0) {
-                //Save the week number into a weeksArray so i can know all the weeks from the new & return visit + i can easly arrange them ...
-                //If the week number not exist then i will add it so i will have only unique numbers ...
-                var wResult = $.inArray(weeks.weekName, weeksArray);
-                if (wResult === -1) {
-                    //Its mean this is a new week number that doesnt exist in the new vist ...
-                    weeksArray.push(weekName);
+//This function is responsible for building the table header ...
+function BuildTableColumn(columnId, columnName, columnColor) {
+    columnNameArray.push({
+        field: columnId,
+        title: i18n.gettext(columnName),
+        align: "center",
+        sortable: true,
+        sortName: columnId,
+        valign: "middle",
+        cellStyle: function cellStyle(value, row, index) {
+            return {
+                css: {
+                    "color": columnColor
                 }
+            };
+        }
+    });
+}
+
+
+
+function BuildClinicNewVisits(clinicName, weeks) {
+    var dataObject = {};
+    var currentWeek = week;
+    //Add Clinic Name ...
+    dataObject.clinicName = clinicName;
+
+    for (i = currentWeek - 2; i <= currentWeek; i++) {
+        var itemName = i + "N";
+        dataObject[itemName] = weeks[i];
+
+        //Add default value to the return visits...
+        var itemNameF = i + "F";
+        dataObject[itemNameF] = 0;
+
+        //Add default value to the Completeness results...
+        var itemNameC = i + "C";
+        dataObject[itemNameC] = 0;
+
+    }
+    clinicDataArray.push(dataObject);
+}
+
+
+
+function BuildClinicReturnVisits(clinicName, weeks) {
+    var currentWeek = week;
+    $.each(clinicDataArray, function(index, value) {
+        if (value.clinicName.toString() === clinicName.toString()) {
+            for (i = currentWeek - 2; i <= currentWeek; i++) {
+                var itemName = i + "F";
+                value[itemName] = weeks[i];
             }
-        } else {
-            newVisitArray.push(weeks.weekName);
         }
-
     });
 }
 
-function BuildClinicHTMLContent() {
-    //Sort the weeks array Ascending ...
-    SortClinicsWeeks(weeksArray);
 
-    //Build the Title for the Table ..
-    var html_builder = "<div  class='col-xs-12 col-md-12 less-padding-col'> <div class='chartBox box' >" +
-        "<div class = 'chartBox__heading' > <p id = '#box_heading'>" + "Clinics Entries" + "</p> </div>" +
-        "<div class = 'chartBox__content' > " +
-        "<div class='divTable'><div  class='divTableBody' >";
-
-    //Build the header ex: Name of facility - Week 1 (new) - week 1 (folow up) ...
-    html_builder = html_builder + "<div class='divTableRow'>" + "<div class='divTableCell' style='font-weight: bold;'> " + "Name Of Facility" + "</div>";
-    $.each(weeksArray, function(index, value) {
-
-        //No need to redundant the code just add variables and fill it if its the current week ...
-        var columnRedStyle = "";
-        var currentText = "";
-        if (value === week.toString()) {
-            columnRedStyle = "color: red;";
-            currentText = " current ";
+//This function will get the completness value and put it in the right position in the array ...
+function BuildClinicCompleteness(clinicName, weeks) {
+    var currentWeek = week;
+    var counter = 3; // i need the last 3 values only which represent the last 3 weeks ...
+    $.each(clinicDataArray, function(index, value) {
+        if (value.clinicName.toString() === clinicName.toString()) {
+            for (i = currentWeek - 2; i <= currentWeek; i++) {
+                var itemName = i + "C";
+                value[itemName] = CalculateCompleteness(weeks[weeks.length - counter]) + ' % ';
+                counter = counter - 1;
+            }
         }
-
-        html_builder = html_builder + "<div class='divTableCell' style='font-weight: bold;" + columnRedStyle + "'> " + "Week " + value + currentText + " (new) " + "</div>";
-        html_builder = html_builder + "<div class='divTableCell' style='font-weight: bold;" + columnRedStyle + "'> " + "Week " + value + currentText + " (follow up) " + "</div>";
-        //  html_builder = html_builder + "<div class='divTableCell' style='font-weight: bold;" + columnRedStyle + "'> " + "Week " + value + " ( completeness ) " + "</div>";
     });
-    html_builder = html_builder + "</div>";
+}
 
 
-    // Draw the Values ..
-    $.each(globalArray, function(ind, clinicName) {
-        //Draw Clinic Name ...
-        html_builder = html_builder + "<div class='divTableRow'>" + "<div class='divTableCell' style='font-weight: bold;'> " + clinicName + "</div>";
 
-        //I will move into the weeks column to  make check if the week exist in the current clinic , yes draw values , No draw "0" ...
-        $.each(weeksArray, function(i, weekNumber) {
-            var foundWeek = 0;
+//This function will overwrite the clinic number with the name ...
+function GetClinicsNames(data) {
+    $.each(clinicDataArray, function(index, value) {
+        value.clinicName = data[value.clinicName].name;
+    });
+}
 
-            $.each(globalArray[clinicName], function(i, week) {
 
-                if (week[0] === weekNumber) {
-                    foundWeek = 1;
-                    html_builder = html_builder + "<div class='divTableCell' style='font-weight: bold;'> " + week[1] + "</div>";
-                    html_builder = html_builder + "<div class='divTableCell' style='font-weight: bold;'> " + week[2] + "</div>";
-                } else {
-                    if (i + 1 === globalArray[clinicName].length && foundWeek === 0) {
-                        html_builder = html_builder + "<div class='divTableCell' style='font-weight: bold;'> " + "0" + "</div>";
-                        html_builder = html_builder + "<div class='divTableCell' style='font-weight: bold;'> " + "0" + "</div>";
-                    }
-                }
 
-            });
+//This Method will return the %  percentage ...
+function CalculateCompleteness(clinicSubmitNum) {
+    var result = 100 * (clinicSubmitNum / config.completeness_denominator.all); // "config" is a global variable ...
+    if (result > 100) {
+        return 100;
+    }
+    return parseInt(result);
+}
 
+
+
+//This Function will update the column Style so the text will not be in one line
+function ChangeHeaderCss() {
+    $('div.th-inner').each(function() {
+        $(this).css({
+            'white-space': "normal"
         });
-
-        html_builder = html_builder + "</div>";
-    });
-
-
-    //Build the box footer ...
-    html_builder = html_builder + "</div> </div> </div> </div> </div>";
-
-
-    //Add the HTML into the Clinic.html ...
-    $('#divClinicContent').append(html_builder);
-
-}
-
-function SortClinicsWeeks(arr) {
-    arr.sort(function(a, b) {
-        if (isNaN(a) || isNaN(b)) {
-            return a > b ? 1 : -1;
-        }
-        return a - b;
     });
 }
