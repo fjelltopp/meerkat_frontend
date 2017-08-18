46c384f3bdac48e4ffb28a887086a88ce65927aa
Ziad Abu Ghazaleh <ziad0>
2017-08-18 10:51:36 +0300

    Redesign the Indicator Box inside the overview page and add chart to it

--- meerkat_frontend/src/js/technical/overview.js
+++ meerkat_frontend/src/js/technical/overview.js
@@ -37,18 +37,18 @@ function html_box_builder(overviewObj, locID) {
 function prep_row(contentsObj, parentId, locID) {
     if (isUserAthorized(contentsObj.access) === true) {
 
-		var ovPeriodType = "year";
-		if ( contentsObj.prep_details !== undefined){
-			ovPeriodType = contentsObj.prep_details.ovPeriodType;
-		}
-		
+        var ovPeriodType = "year";
+        if (contentsObj.prep_details !== undefined) {
+            ovPeriodType = contentsObj.prep_details.ovPeriodType;
+        }
+
         //Generate a GUID ...
         var api_element = generateGUID();
 
         //Append the results ...
         var htmlRow = "<div class='divTableRow'>" +
             "<div class='divTableCell' style='font-weight: bold;'> " + i18n.gettext(contentsObj.label) + "</div>" +
-            "<div class='divTableCell " + api_element + "'> "+ i18n.gettext("Loading") +"..</div>" +
+            "<div class='divTableCell " + api_element + "'> " + i18n.gettext("Loading") + "..</div>" +
             "</div>";
 
         $("#" + parentId).append(htmlRow);
@@ -56,11 +56,11 @@ function prep_row(contentsObj, parentId, locID) {
         // Get the inner value for the boxes by calling the APIs ...
         var apiUrl = contentsObj.api.replace("<loc_id>", locID);
         $.getJSON(api_root + apiUrl, function(data) {
-			if(ovPeriodType == "weeks"){
-				$('#' + parentId + ' .' + api_element).html(if_exists(data[ovPeriodType], week));
-			}else{
-				$('#' + parentId + ' .' + api_element).html(data[ovPeriodType]);
-			}
+            if (ovPeriodType == "weeks") {
+                $('#' + parentId + ' .' + api_element).html(if_exists(data[ovPeriodType], week));
+            } else {
+                $('#' + parentId + ' .' + api_element).html(data[ovPeriodType]);
+            }
 
         });
 
@@ -222,28 +222,45 @@ function prep_row_draw_Last3(contentsObj, parentId, locID) {
 }
 
 
+var indicatorCounter = 0;
+//Generate a GUID ...
+var api_element_ind = generateGUID();
+
 function prep_row_indicator(contentsObj, parentId, locID) {
     if (isUserAthorized(contentsObj.access) === true) {
 
-        //Generate a GUID ...
-        var api_element = generateGUID();
+        var htmlContetn = "";
+        //Add the table structure for the first time ...
+        if (indicatorCounter === 0) {
+            htmlContetn = "<table id='table-sparkline' style='width:100%'><thead><tr><th>Indicator name</th><th>cumulative</th><th>Indicator chart</th></tr></thead>" +
+                "<tbody id='tbody-sparkline' name=" + api_element_ind + ">" + "</tbody></table>";
+            indicatorCounter = 1;
+            $("#" + parentId).append(htmlContetn);
+        }
 
-        //Append the results ...
-        var htmlRow = "<div class='divTableRow'>" +
-            "<div class='divTableCell' style='font-weight: bold;'> " + contentsObj.label + "</div>" +
-            "<div class='divTableCell " + api_element + "'>Loading..</div>" +
-            "</div>";
-
-        $("#" + parentId).append(htmlRow);
 
         // Get the inner value for the boxes by calling the APIs ...
         var apiUrl = contentsObj.api.replace("<loc_id>", locID);
         $.getJSON(api_root + apiUrl, function(data) {
 
-            $('#' + parentId + ' .' + api_element).html(round(data.current, 1) + "<span style='padding-left:30px;' class='" + getIndicatorArrow (data.current - data.previous) + "'></span> ");
+            var indicatorColumn = "";
+            var indicatorChartValues = [];
+
+            $.each(data.timeline, function(index, value) {
+                indicatorChartValues.push(value);
+            });
+
+            indicatorColumn = "<tr><th> " + contentsObj.label + "</th>" + "<td>" + data.current + "% (" + data.cummulative + "% year )" + "</td>" +
+            "<td data-sparkline = '" + indicatorChartValues.join(", ") + "'" +
+            "' onClick=showIndicatorChart('" + apiUrl.toString() + "'); /> </tr>";
+          //  "<td data-sparkline = '" + indicatorChartValues.join(", ") + "' onClick=showIndicatorChart('" + apiUrl.toString() + "'); /> </tr>";
+
+
+
+            $("tbody[name='" + api_element_ind + "']").append(indicatorColumn);
+            drawIndicatorChart();
 
         });
-
     }
 }
 
@@ -288,9 +305,241 @@ function ovDateFormate(dateStr) {
 function getIndicatorArrow(val) {
     if (val > 0) {
         return "glyphicon glyphicon-arrow-up";
-    }else if( val === 0){
-		return "glyphicon glyphicon-minus";
-	}else {
+    } else if (val === 0) {
+        return "glyphicon glyphicon-minus";
+    } else {
         return "glyphicon glyphicon-arrow-down";
     }
+}
+
+
+function drawIndicatorChart() {
+    /**
+     * Create a constructor for sparklines that takes some sensible defaults and merges in the individual
+     * chart options. This function is also available from the jQuery plugin as $(element).highcharts('SparkLine').
+     */
+    Highcharts.SparkLine = function(a, b, c) {
+        var hasRenderToArg = typeof a === 'string' || a.nodeName,
+            options = arguments[hasRenderToArg ? 1 : 0],
+            defaultOptions = {
+                chart: {
+                    renderTo: (options.chart && options.chart.renderTo) || this,
+                    backgroundColor: null,
+                    borderWidth: 0,
+                    type: 'area',
+                    margin: [2, 0, 2, 0],
+                    width: 120,
+                    height: 20,
+                    style: {
+                        overflow: 'visible'
+                    },
+
+                    // small optimalization, saves 1-2 ms each sparkline
+                    skipClone: true
+                },
+                title: {
+                    text: ''
+                },
+                credits: {
+                    enabled: false
+                },
+                xAxis: {
+                    labels: {
+                        enabled: false
+                    },
+                    title: {
+                        text: null
+                    },
+                    startOnTick: false,
+                    endOnTick: false,
+                    tickPositions: []
+                },
+                yAxis: {
+                    endOnTick: false,
+                    startOnTick: false,
+                    labels: {
+                        enabled: false
+                    },
+                    title: {
+                        text: null
+                    },
+                    tickPositions: [0]
+                },
+                legend: {
+                    enabled: false
+                },
+                tooltip: {
+                    backgroundColor: null,
+                    borderWidth: 0,
+                    shadow: false,
+                    useHTML: true,
+                    hideDelay: 0,
+                    shared: true,
+                    padding: 0,
+                    positioner: function(w, h, point) {
+                        return {
+                            x: point.plotX - w / 2,
+                            y: point.plotY - h
+                        };
+                    }
+                },
+                plotOptions: {
+                    series: {
+                        animation: false,
+                        lineWidth: 1,
+                        shadow: false,
+                        states: {
+                            hover: {
+                                lineWidth: 1
+                            }
+                        },
+                        marker: {
+                            radius: 1,
+                            enabled: false,
+                            states: {
+                                hover: {
+                                    radius: 2
+                                }
+                            }
+                        },
+                        fillOpacity: 0.25
+                    },
+                    column: {
+                        negativeColor: '#910000',
+                        borderColor: 'silver'
+                    }
+                }
+            };
+
+        options = Highcharts.merge(defaultOptions, options);
+
+        return hasRenderToArg ?
+            new Highcharts.Chart(a, options, c) :
+            new Highcharts.Chart(options, b);
+    };
+
+    var start = +new Date(),
+        $tds = $('td[data-sparkline]'),
+        fullLen = $tds.length,
+        n = 0;
+
+    doChunk();
+    // Creating 153 sparkline charts is quite fast in modern browsers, but IE8 and mobile
+    // can take some seconds, so we split the input into chunks and apply them in timeouts
+    // in order avoid locking up the browser process and allow interaction.
+    function doChunk() {
+        var time = +new Date(),
+            i,
+            len = $tds.length,
+            $td,
+            stringdata,
+            arr,
+            data,
+            chart;
+
+        for (i = 0; i < len; i += 1) {
+            $td = $($tds[i]);
+            stringdata = $td.data('sparkline');
+            arr = stringdata.split('; ');
+            data = $.map(arr[0].split(', '), parseFloat);
+            chart = {};
+
+            if (arr[1]) {
+                chart.type = arr[1];
+            }
+            $td.highcharts('SparkLine', {
+                series: [{
+                    data: data,
+                    pointStart: 1
+                }],
+                tooltip: {
+                    headerFormat: '<span style="font-size: 10px">' + $td.parent().find('th').html() + ', Q{point.x}:</span><br/>',
+                    pointFormat: '<b>{point.y}.000</b>'
+                },
+                chart: chart
+            });
+
+            n += 1;
+
+            // If the process takes too much time, run a timeout to allow interaction with the browser
+            if (new Date() - time > 500) {
+                $tds.splice(0, i + 1);
+                setTimeout(doChunk, 0);
+                break;
+            }
+
+            // Print a feedback on the performance
+            // if (n === fullLen) {
+            //     $('#result').html('Generated ' + fullLen + ' sparklines in ' + (new Date() - start) + ' ms');
+            // }
+        }
+    }
+}
+
+
+function showIndicatorChart(apiUrl) {
+
+    var indicatorChartDate = [];
+    var indicatorChartValues = [];
+
+    $.getJSON(api_root + apiUrl, function(result) {
+        var indicatorChartValues = [];
+
+        $.each(result.timeline, function(index, value) {
+            indicatorChartDate.push(ovDateFormate(index));
+            indicatorChartValues.push(value);
+        });
+
+        Highcharts.chart('indicatorChartlightbox', {
+            chart: {
+                type: 'area'
+            },
+            title: {
+                text: 'Indicators'
+            },
+            subtitle: {
+                text: ''
+            },
+            xAxis: {
+                categories: indicatorChartDate ,
+                tickmarkPlacement: 'on',
+                title: {
+                    enabled: false
+                }
+            },
+            yAxis: {
+                title: {
+                    text: ''
+                },
+                labels: {
+                    formatter: function() {
+                        return this.value / 1000;
+                    }
+                }
+            },
+            tooltip: {
+                split: true,
+                valueSuffix: ' '
+            },
+            plotOptions: {
+                area: {
+                    stacking: 'normal',
+                    lineColor: '#666666',
+                    lineWidth: 1,
+                    marker: {
+                        lineWidth: 1,
+                        lineColor: '#666666'
+                    }
+                }
+            },
+            series: [{
+                name: 'indicator Name',
+                data: indicatorChartValues
+            }]
+        });
+
+        //$.featherlight($('#indicatorChartlightbox'), {});
+var indicatorChartHTMLContainer = $('#indicatorChartlightbox').html();
+ $.featherlight( indicatorChartHTMLContainer, {variant:'indicatorChartHTMLContainer'} );
+    });
 }
