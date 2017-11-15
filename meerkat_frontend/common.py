8fe4ab1ad9fb81f48545be92b85f69878b9bb9fd
Jonathan Berry <jonathan.s.berry@googlemail.com>
2017-11-15 10:47:29 +0000

    Improve logging for debug.

--- meerkat_frontend/common.py
+++ meerkat_frontend/common.py
@@ -43,8 +43,8 @@ def api(url, params=None):
         try:
             output = r.json()
         except Exception as e:
-            logging.error("Failed to convert API response {}"
-                          "to JSON: {}".format(r, r.text))
+            logging.error("Failed to convert API {} response {} "
+                          "to JSON: {}".format(url, r, r.text))
             logging.error(e)
             abort(500, "Failed to convert API response to JSON.")
         return output
