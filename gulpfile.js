148e3b3aba2d8339af659e13f1aec637da9287d8
Jonathan Berry <jonathan.s.berry@googlemail.com>
2018-01-31 11:59:22 +0000

    Fix gulp build after reverts.

--- gulpfile.js
+++ gulpfile.js
@@ -81,7 +81,7 @@ gulp.task('mapbox-rename-css-to-scss', function() {
       'bower_components/mapbox.js/mapbox.uncompressed.css',
       'bower_components/leaflet.markercluster/dist/MarkerCluster.css',
       'bower_components/leaflet.markercluster/dist/MarkerCluster.Default.css',
-      'bower_components/intl-tel-input/build/css/intltelInput.css',
+      'bower_components/intl-tel-input/build/css/intlTelInput.css',
       'bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css',
       'bower_components/bootstrap-table/src/bootstrap-table.css',
       'bower_components/featherlight/src/featherlight.css',
