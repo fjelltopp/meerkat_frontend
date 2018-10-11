d636c9ca92ec6d0dffd297314b30a1b866acf383
Jonathan Berry <jonathan.s.berry@googlemail.com>
2018-10-11 15:11:49 +0100

    Make topics searchable in subscriber table.

--- meerkat_frontend/src/js/messaging/messaging.js
+++ meerkat_frontend/src/js/messaging/messaging.js
@@ -1,8 +1,8 @@
 
-function get_topics(topics){
+function getTopics(topics){
     var ret = {'topics': [], 'locID': 1};
     for( var t in topics){
-      var parts = topics[t].split('-');
+      var parts = String(topics[t]).split('-');
       ret.topics.push(parts[parts.length-1]);
       if( parts.length == 3 ) ret.locID = parseInt(parts[1]);
     }
@@ -73,7 +73,7 @@ function drawForm(subscriber){
       $("#email").val(subscriber.email);
       $("#email2").val(subscriber.email);
       if( subscriber.sms ) $('#sms-number').intlTelInput("setNumber", subscriber.sms);
-      var data = get_topics(subscriber.topics);
+      var data = getTopics(subscriber.topics);
       loadLocation(data.locID);
       for( var t in data.topics ){
         $("#subscribe-form input[name='topics'][value='" + data.topics[t] + "']").prop('checked', true);
@@ -91,139 +91,145 @@ function drawForm(subscriber){
  * from the /subscribers/get_subscribers resource.
 */
 function drawSubscriberTable(){
-
-    //Define the table structure
-    var columns = [{
-            'field': 'state',
-            'checkbox': true,
-            'align': 'center',
-            'valign': 'middle'
-        },{
-            'field': "first_name",
-            'title': i18n.gettext('First Name'),
-            'align': "left",
-            'class': "header",
-            'sortable': true,
-            'width': "20%"
-        },{
-            'field': "last_name",
-            'title': i18n.gettext('Last Name'),
-            'align': "left",
-            'class': "header",
-            'sortable': true,
-            'width': "20%"
-        },{
-            'field': "email",
-            'title': i18n.gettext('Email'),
-            'align': "left",
-            'class': "header",
-            'sortable': true,
-            'width': "20%"
-        },{
-            'field': "sms",
-            'title': i18n.gettext('SMS'),
-            'align': "left",
-            'class': "header",
-            'sortable': true,
-            'width': "20%"
-        },{
-            'field': "verified",
-            'title': i18n.gettext('Verified?'),
-            'align': "left",
-            'class': "header",
-            'sortable': true,
-            'width': "20%"
-        },{
-            'field': "topics",
-            'title': i18n.gettext('Topics'),
-            'align': "left",
-            'class': "header",
-            'sortable': false,
-            'visible': false,
-            'searchable': true,
-            'width': "25%"
-        }
-    ];
-
-    //A function that prepares the data for displaying in the table.
-    function prepData(res){
-        // for( var a in res.rows ){
-        //
-        // }
-        return res.rows;
-    }
-
-    var tmp = '/en/messaging/get_subscribers';
-    console.log( "tmp: " + tmp );
-
-    //Create the bootstrap table.
-    table = $('#subscriber-table table').bootstrapTable({
-        columns: columns,
-        classes: 'table table-no-bordered table-hover',
-        pagination: true,
-        pageSize: 20,
-        search: true,
-        url: tmp,
-        responseHandler: prepData,
-        showRefresh: true
-    });
-
-    //Insert data into editor when clicking upon a row.
-    $('#subscriber-table table').on('click-row.bs.table', function (row, $element, field) {
-        drawForm($element);
-    });
-
-    //Add extra toolbar buttons
-    var buttons = "<div class='btn-group  pull-right table-custom-toolbar'>" +
-        "<button class='btn highlight delete-subscribers' type='button'>" +
-        "<span class='glyphicon glyphicon-trash'/></button>" +
-        "<button class='btn blue new-subscriber' type='button'>" +
-        "<span class='glyphicon glyphicon-plus'/></button>" +
-        "</div>";
-    $('.fixed-table-toolbar').append(buttons);
-
-    //Enable the new subscriber button.
-    $('button.new-subscriber').click( function(){
-        drawForm();
-    });
-
-    //Enable the delete subscriber button.
-    $('button.delete-subscribers').click( function(){
-
-        //First of all extract the subscriberids and names of the subscribers to be deleted.
-        var selected = $('#subscriber-table table').bootstrapTable('getSelections');
-        var subscriberids = [];
-        var confirmString = i18n.gettext("Are you sure you want to delete the following subscribers?") +
-            "\n";
-        for( var subscriber in selected ){
-            subscriberids.push( selected[subscriber].id );
-            confirmString = confirmString + selected[subscriber].first_name +
-                            " " + selected[subscriber].last_name + ", ";
-        }
-
-        //If there are no subscriberids, just ignore.
-        if( subscriberids.length > 0 ){
-            //Check with the subscriber before doing the deletion.
-            if( confirm(confirmString.slice(0, -2)) ){
-                //Do the deletion by posting json to server.
-                $.ajax({
-                    url: '/en/messaging/delete_subscribers',
-                    type: 'post',
-                    success: function (data) {
-                        alert(data);
-                        $('#subscriber-table table').bootstrapTable('refresh');
-                        drawForm();
-                    },
-                    error: function (data) {
-                        alert( i18n.gettext( "There has been a server error. " +
-                               "Please contact administrator and try again later." ) );
-                        $('.subscriber-editor .submit-form').text( buttonText );
-                    },
-                    contentType: 'application/json;charset=UTF-8',
-                    data: JSON.stringify(subscriberids, null, '\t')
-                });
+    $.getJSON(api_root+"/variables/alert?include_group_b=1", function(variables){
+        //Define the table structure
+        var columns = [{
+                'field': 'state',
+                'checkbox': true,
+                'align': 'center',
+                'valign': 'middle'
+            },{
+                'field': "first_name",
+                'title': i18n.gettext('First Name'),
+                'align': "left",
+                'class': "header",
+                'sortable': true,
+                'width': "20%"
+            },{
+                'field': "last_name",
+                'title': i18n.gettext('Last Name'),
+                'align': "left",
+                'class': "header",
+                'sortable': true,
+                'width': "20%"
+            },{
+                'field': "email",
+                'title': i18n.gettext('Email'),
+                'align': "left",
+                'class': "header",
+                'sortable': true,
+                'width': "20%"
+            },{
+                'field': "sms",
+                'title': i18n.gettext('SMS'),
+                'align': "left",
+                'class': "header",
+                'sortable': true,
+                'width': "20%"
+            },{
+                'field': "verified",
+                'title': i18n.gettext('Verified?'),
+                'align': "left",
+                'class': "header",
+                'sortable': true,
+                'width': "20%"
+            },{
+                'field': "topicStrings",
+                'title': i18n.gettext('Topics'),
+                'visible': false,
+                'searchable': true
             }
+        ];
+
+        var tmp = '/en/messaging/get_subscribers';
+
+        for(var r in config.subscribe.reports){
+            var report = config.subscribe.reports[r];
+            variables[report.id] = report;
         }
+        variables.allDis = {'name': i18n.gettext('All Alerts')};
+
+        //A function that prepares the data for displaying in the table.
+        function prepData(res){
+            for(var s in res.rows){
+                var topics = getTopics(res.rows[s].topics).topics;
+                res.rows[s].topicStrings = topics.map(function(topic){
+                    return i18n.gettext(variables[topic].name) || topic;
+                }).join(', ');
+            }
+            return res.rows;
+        }
+
+        //Create the bootstrap table.
+        table = $('#subscriber-table table').bootstrapTable({
+            columns: columns,
+            classes: 'table table-no-bordered table-hover',
+            pagination: true,
+            pageSize: 20,
+            search: true,
+            url: tmp,
+            responseHandler: prepData,
+            showRefresh: true
+        });
+
+        //Insert data into editor when clicking upon a row.
+        $('#subscriber-table table').on('click-row.bs.table', function (row, $element, field) {
+            drawForm($element);
+            scrollToID('#subscribe-box', 750);
+        });
+
+        //Add extra toolbar buttons
+        var buttons = "<div class='btn-group  pull-right table-custom-toolbar'>" +
+            "<button class='btn highlight delete-subscribers' type='button'>" +
+            "<span class='glyphicon glyphicon-trash'/></button>" +
+            "<button class='btn blue new-subscriber' type='button'>" +
+            "<span class='glyphicon glyphicon-plus'/></button>" +
+            "</div>";
+        $('.fixed-table-toolbar').append(buttons);
+
+        //Enable the new subscriber button.
+        $('button.new-subscriber').click( function(){
+            drawForm();
+        });
+
+        //Enable the delete subscriber button.
+        $('button.delete-subscribers').click( function(){
+
+            //First of all extract the subscriberids and names of the subscribers to be deleted.
+            var selected = $('#subscriber-table table').bootstrapTable('getSelections');
+            var subscriberids = [];
+            var confirmString = i18n.gettext("Are you sure you want to delete the following subscribers?") +
+                "\n";
+            for( var subscriber in selected ){
+                subscriberids.push( selected[subscriber].id );
+                confirmString = confirmString + selected[subscriber].first_name +
+                                " " + selected[subscriber].last_name + ", ";
+            }
+
+            //If there are no subscriberids, just ignore.
+            if( subscriberids.length > 0 ){
+                //Check with the subscriber before doing the deletion.
+                if( confirm(confirmString.slice(0, -2)) ){
+                    //Do the deletion by posting json to server.
+                    $.ajax({
+                        url: '/en/messaging/delete_subscribers',
+                        type: 'post',
+                        success: function (data) {
+                            alert(data);
+                            $('#subscriber-table table').bootstrapTable('refresh');
+                            drawForm();
+                        },
+                        error: function (data) {
+                            alert( i18n.gettext( "There has been a server error. " +
+                                   "Please contact administrator and try again later." ) );
+                            $('.subscriber-editor .submit-form').text( buttonText );
+                        },
+                        contentType: 'application/json;charset=UTF-8',
+                        data: JSON.stringify(subscriberids, null, '\t')
+                    });
+                }
+            }
+        });
     });
 }
 
@@ -233,4 +239,12 @@ function toggleAll(source){
 		this.checked = source.checked;
 		this.disabled = source.checked ? true : false;
 	});
+}
+
+// scroll function
+function scrollToID(id, speed){
+    var offSet = 50;
+    var targetOffset = $(id).offset().top - offSet;
+    var mainNav = $('#main-nav');
+    $('html,body').animate({scrollTop:targetOffset}, speed);
 }
