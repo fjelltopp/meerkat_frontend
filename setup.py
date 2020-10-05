9aa67c9f99715411381786375df6a1c96ca5a30a
Tomasz Sabała <sabala.tomek@gmail.com>
2020-10-05 13:37:24 +0200

    Updated parsing requirments.txt in setup.py to newest pip.

--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python3
 import uuid
 from setuptools import setup
-try:  # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError:  # for pip <= 9.0.3
-    from pip.req import parse_requirements
+import pathlib
+import pkg_resources
 
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements("requirements.txt", session=uuid.uuid1())
-
-# reqs is a list of requirement
-# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
-
-reqs = [str(ir.req) for ir in install_reqs if ir.req is not None]
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    reqs = [
+        str(requirement)
+        for requirement
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
 
 setup(
     name='Meerkat Frontend',
